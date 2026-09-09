9fad8c0aa020f19b92edb7c40beda7b6d05a05d9
Sᴀɴᴋᴇᴛ 🖤 <mrsanketpadhyal@gmail.com>
2026-09-09 11:17:13 +0530

    Back link

--- src/api/globalbackendapi.js
+++ src/api/globalbackendapi.js
@@ -1,4 +1,4 @@
-export const GLOBAL_BACKEND_URL = "https://provenbackend.themedora.in";
+export const GLOBAL_BACKEND_URL = "https://backend.theproven.in";
 export const API_BASE_URL = process.env.REACT_APP_API_URL || GLOBAL_BACKEND_URL;
 
 export default API_BASE_URL;
